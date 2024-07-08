0f20108252cb6b3a93d557d28f7d00b0d2842bfa
YusungAN <52990109+YusungAN@users.noreply.github.com>
2024-07-08 16:54:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ let prevY = 0;
 let prevTime = 0;
 
 const neckAnimationMobile = (e) => {
+    if (isMoving) return;
     let nowY = e.touches[0].pageY;
     if (prevY == 0) {
         prevY = nowY;
@@ -141,7 +142,10 @@ const neckAnimationMobile = (e) => {
     } 
 }
 
+let isMoving = false;
+let ivID = 0;
 const touchendEvent = (e) => {
+    if (isMoving) return;
     let nowY = e.touches[0].pageY;
     if (prevY == 0) {
         prevY = nowY;
@@ -158,8 +162,44 @@ const touchendEvent = (e) => {
     if (window.scrollY == 0) outOfMain = false;
     if (!outOfMain) {
         let dt = Date.now() - prevTime;
-        offsetSum += deltaY > 0 ? deltaY/dt : -deltaY/dt;
-        prevTime = 0;
+        if (dt < 300) {
+            isMoving = true;
+            ivID = setInterval(() => {
+                offsetSum += 5;
+                if (vw > 900) {
+                    neck.style.transform = `rotate(${offsetSum/500*30}deg)`;
+                    neckHead.style.top = `${-24 - Math.cos(offsetSum/500*30*Math.PI/180)*8}vh`;
+                    neckHead.style.left = `${3.75 + Math.sin(offsetSum/500*30*Math.PI/180)*8}vh`;
+                } else {
+                    neck.style.transform = `rotate(${offsetSum/500*30}deg)`;
+                    neckHead.style.top = `${-19 - Math.cos(offsetSum/500*30*Math.PI/180)*6}vh`;
+                    neckHead.style.left = `${2.5 + Math.sin(offsetSum/500*30*Math.PI/180)*6}vh`;
+                }
+                if (offsetSum >= 500) {
+                    document.getElementsByTagName('body')[0].style.position = 'static';
+                    outOfMain = true;
+                    clearInterval(ivID);
+                }
+                if (offsetSum >= 300) {
+                    bg.style.backgroundColor = '#FFC2B9';
+                    if (!showedLogo) {
+                        if (vw > 900) {
+                            noti.style.animationName = 'fadein1';
+                            noti.style.animationDuration = '1s';
+                            noti.style.opacity = 1;
+                        } else {
+                            noti.style.animationName = 'fadein2';
+                            noti.style.animationDuration = '1s';
+                            noti.style.opacity = 1;
+                        }
+                        showedLogo = true;
+                    }
+                } else {
+                    bg.style.backgroundColor = '#BBFFB9';
+                }
+            }, 10);
+
+        }
     }
     
     if (offsetSum < 0) {
